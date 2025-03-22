a7cc684fad4ab2090de025048db053372d19ed8a
camtisocial <thompsonca99@gmail.com>
2025-03-21 21:52:27 -0500

    added blender model and lighting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import SceneCanvas from './hooks/SceneCanvas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+      <SceneCanvas />
+  </div>
   );
 }
 
 export default App;
+      // <div class="App-header">Hello, 3D World!</div>
--- src/hooks/SceneCanvas.js
+++ src/hooks/SceneCanvas.js
@@ -1,34 +1,68 @@
 import React, { useEffect, useRef } from 'react';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import * as THREE from 'three';
 
 const SceneCanvas = () => {
+
+  // create a ref to the scene, this will be used to append the renderer to the DOM react style
+  const mountRef = useRef(null);
+
+  useEffect(() => {
   // create scene and camera
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+
   // create/configure renderer then append to DOM
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-   
-  // create a cube and add to scene
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  renderer.setClearColor(0x000000, 1);
+
+  // append renderer to DOM
+  if (mountRef.current) {
+    mountRef.current.appendChild(renderer.domElement);
+  }
+
+  // create a light
+  const light = new THREE.DirectionalLight(0xffffff, 5);
+  light.position.set(0, 0, 10);
+  scene.add(light);
+
+  
+  // create a loader for pulling in a .glb or .gltf file made in Blender
+  const loader = new GLTFLoader();
+  loader.load(
+    '/spin.glb',
+    function(gltf) {
+      const model = gltf.scene;
+      model.position.set(0, 0, 0);
+      model.scale.set(.8, .8, .8);
+      scene.add(model);
+    }, undefined, function(error) {
+    console.error(error);
+  });
 
   // position camera
   camera.position.z = 5;
+  camera.position.y = 1;
 
-  //function to call animation
+  //function to call animation loop
   function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render(scene, camera);
   }
-
   renderer.setAnimationLoop(animate);
 
+  // clean up function
+  return () => {
+    if (mountRef.current) {
+      mountRef.current.removeChild(renderer.domElement);
+    }
+    renderer.dispose();
+  };
+
+}, []);
+  
+  return <div ref={mountRef} />;
 };
 
 export default SceneCanvas;
